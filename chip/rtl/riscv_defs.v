02950ce4beed6c60424af63b4847d9f9b3e0df42
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2022-10-14 12:19:06 +0200

    Simplify decoding with masking

---
+++ chip/rtl/riscv_defs.v
@@ -0,0 +1,225 @@
+//-----------------------------------------------------------------
+//                         RISC-V Core
+//                            V1.0.1
+//                     Ultra-Embedded.com
+//                     Copyright 2014-2019
+//
+//                   admin@ultra-embedded.com
+//
+//                       License: BSD
+//-----------------------------------------------------------------
+//
+// Copyright (c) 2014-2019, Ultra-Embedded.com
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions 
+// are met:
+//   - Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer 
+//     in the documentation and/or other materials provided with the 
+//     distribution.
+//   - Neither the name of the author nor the names of its contributors 
+//     may be used to endorse or promote products derived from this 
+//     software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+// SUCH DAMAGE.
+//--------------------------------------------------------------------
+// Instructions Masks
+//--------------------------------------------------------------------
+// andi
+`define INST_ANDI 32'h7013
+`define INST_ANDI_MASK 32'h707f
+
+// addi
+`define INST_ADDI 32'h13
+`define INST_ADDI_MASK 32'h707f
+
+// slti
+`define INST_SLTI 32'h2013
+`define INST_SLTI_MASK 32'h707f
+
+// sltiu
+`define INST_SLTIU 32'h3013
+`define INST_SLTIU_MASK 32'h707f
+
+// ori
+`define INST_ORI 32'h6013
+`define INST_ORI_MASK 32'h707f
+
+// xori
+`define INST_XORI 32'h4013
+`define INST_XORI_MASK 32'h707f
+
+// slli
+`define INST_SLLI 32'h1013
+`define INST_SLLI_MASK 32'hfc00707f
+
+// srli
+`define INST_SRLI 32'h5013
+`define INST_SRLI_MASK 32'hfc00707f
+
+// srai
+`define INST_SRAI 32'h40005013
+`define INST_SRAI_MASK 32'hfc00707f
+
+// lui
+`define INST_LUI 32'h37
+`define INST_LUI_MASK 32'h7f
+
+// auipc
+`define INST_AUIPC 32'h17
+`define INST_AUIPC_MASK 32'h7f
+
+// add
+`define INST_ADD 32'h33
+`define INST_ADD_MASK 32'hfe00707f
+
+// sub
+`define INST_SUB 32'h40000033
+`define INST_SUB_MASK 32'hfe00707f
+
+// slt
+`define INST_SLT 32'h2033
+`define INST_SLT_MASK 32'hfe00707f
+
+// sltu
+`define INST_SLTU 32'h3033
+`define INST_SLTU_MASK 32'hfe00707f
+
+// xor
+`define INST_XOR 32'h4033
+`define INST_XOR_MASK 32'hfe00707f
+
+// or
+`define INST_OR 32'h6033
+`define INST_OR_MASK 32'hfe00707f
+
+// and
+`define INST_AND 32'h7033
+`define INST_AND_MASK 32'hfe00707f
+
+// sll
+`define INST_SLL 32'h1033
+`define INST_SLL_MASK 32'hfe00707f
+
+// srl
+`define INST_SRL 32'h5033
+`define INST_SRL_MASK 32'hfe00707f
+
+// sra
+`define INST_SRA 32'h40005033
+`define INST_SRA_MASK 32'hfe00707f
+
+// jal
+`define INST_JAL 32'h6f
+`define INST_JAL_MASK 32'h7f
+
+// jalr
+`define INST_JALR 32'h67
+`define INST_JALR_MASK 32'h707f
+
+// beq
+`define INST_BEQ 32'h63
+`define INST_BEQ_MASK 32'h707f
+
+// bne
+`define INST_BNE 32'h1063
+`define INST_BNE_MASK 32'h707f
+
+// blt
+`define INST_BLT 32'h4063
+`define INST_BLT_MASK 32'h707f
+
+// bge
+`define INST_BGE 32'h5063
+`define INST_BGE_MASK 32'h707f
+
+// bltu
+`define INST_BLTU 32'h6063
+`define INST_BLTU_MASK 32'h707f
+
+// bgeu
+`define INST_BGEU 32'h7063
+`define INST_BGEU_MASK 32'h707f
+
+// lb
+`define INST_LB 32'h3
+`define INST_LB_MASK 32'h707f
+
+// lh
+`define INST_LH 32'h1003
+`define INST_LH_MASK 32'h707f
+
+// lw
+`define INST_LW 32'h2003
+`define INST_LW_MASK 32'h707f
+
+// lbu
+`define INST_LBU 32'h4003
+`define INST_LBU_MASK 32'h707f
+
+// lhu
+`define INST_LHU 32'h5003
+`define INST_LHU_MASK 32'h707f
+
+// lwu
+`define INST_LWU 32'h6003
+`define INST_LWU_MASK 32'h707f
+
+// sb
+`define INST_SB 32'h23
+`define INST_SB_MASK 32'h707f
+
+// sh
+`define INST_SH 32'h1023
+`define INST_SH_MASK 32'h707f
+
+// sw
+`define INST_SW 32'h2023
+`define INST_SW_MASK 32'h707f
+
+// mul
+`define INST_MUL 32'h2000033
+`define INST_MUL_MASK 32'hfe00707f
+
+// mulh
+`define INST_MULH 32'h2001033
+`define INST_MULH_MASK 32'hfe00707f
+
+// mulhsu
+`define INST_MULHSU 32'h2002033
+`define INST_MULHSU_MASK 32'hfe00707f
+
+// mulhu
+`define INST_MULHU 32'h2003033
+`define INST_MULHU_MASK 32'hfe00707f
+
+// div
+`define INST_DIV 32'h2004033
+`define INST_DIV_MASK 32'hfe00707f
+
+// divu
+`define INST_DIVU 32'h2005033
+`define INST_DIVU_MASK 32'hfe00707f
+
+// rem
+`define INST_REM 32'h2006033
+`define INST_REM_MASK 32'hfe00707f
+
+// remu
+`define INST_REMU 32'h2007033
+`define INST_REMU_MASK 32'hfe00707f
